c4a64eb4091fc90eb836757f159bc17620a43eaa
= <=>
2021-03-24 20:27:52 -0700

    added chunks of array code

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,33 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(stringA, stringB) {
+  const stringA_Map = {};
+  const stringB_Map = {};
 
+  if (stringA.length !== stringB.length) {
+    return false;
+  }
+
+  for (let char of stringA) {
+    if(stringA_Map[char]) {
+      stringA_Map[char] = stringA_Map[char] + 1
+    } else {
+      stringA_Map[char] = 1;
+    }
+  }
+
+  for (let char of stringB) {
+    if(stringB_Map[char]) {
+      stringB_Map[char] = stringB_Map[char] + 1
+    } else {
+      stringB_Map[char] = 1;
+    }
+  }
+  
+  console.log( 'string A mapping - ', stringA_Map);
+  console.log( 'string B mapping - ', stringB_Map);
+}
+
+anagrams('rail safety', 'fairy tales')
 module.exports = anagrams;
--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,21 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+const chunk = (array, size) => {
+  const finalResults = [];
+  const chunks = [];
+  let index = 0;
 
+  // [0,1,2,3,4] = length 5
+  // index = 0,1,2,3,4
+  while (index < array.length) {
+    finalResults.push(array.slice(index, index + size));
+    index += size
+  }
+
+  return finalResults;
+}
+
+const results = chunk([1, 2, 3, 4, 5, 6, 7, 8], 3);
+console.log('Results : ', results);
 module.exports = chunk;
