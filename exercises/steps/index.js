9d8d8d46e9eb8ba54188a27b214cfac52eeae2cf
= <=>
2021-03-24 21:53:22 -0700

    Added steps code changes

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,6 +17,31 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+const steps = (n) => {
+  /**
+   * input : consider n value 3.
+   * [00, 01, 02]
+   * [10, 11, 12] 
+   * [20, 21, 22]
+   * 
+   * output 
+   * [#, 0, 0]
+   * [#, #, 0]
+   * [#, #, #]
+  */
 
+  for (let row = 0 ; row < n; row++) {
+    let rowString = '';
+    for(let col =0; col < n ; col++){
+      if (col <= row) {
+        rowString = rowString + '#';
+      } else {
+        rowString = rowString + ' ';
+      }
+    }
+    console.log(rowString);
+  }
+}
+
+steps(10);
 module.exports = steps;
