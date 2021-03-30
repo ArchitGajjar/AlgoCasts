f7be04a4dfc1b099847eecada980fea2ab8b9413
= <=>
2021-03-29 21:43:32 -0700

    Finished up Pyramid prob

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,6 +14,23 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+const pyramid = (n) => {
+  // if level = 3 then rows [0,1,2]
+  // column = 5. ( n * 2 - 1)
+  const midpoint = Math.floor((2 * n - 1)/ 2);
+  for(let row=0; row < n; row++){
+    let level = '';
+    for(let col=0; col < 2 * n -1; col++){
+      if(midpoint - row <= col && midpoint + row >= col){
+        level += '#';
+      } else {
+        level += ' ';
+      } 
+    }
+    console.log(level);
+  }
+}
+
+pyramid(10); 
 
 module.exports = pyramid;
